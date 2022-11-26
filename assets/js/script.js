a1e72ee291f2a9f63bdc2a98a5385039d5b2d866
ankasendo <anka.sendo@gmail.com>
2022-11-26 12:12:23 +0000

    add questions to Js file

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+const quizData = [{
+        question: "Who are Kylo Ren parents?",
+        a: "Padme and Anakin Skywalker",
+        b: "Han Solo and princess Leia",
+        c: "R2-D2 and C-3PO",
+        d: "Mara Jade and Luke Skywalker",
+        correct: "b"
+
+    },
+    {
+        question: "What is the name of Han Solo ship?",
+        a: "Millennium Falcon",
+        b: "Bird Rocket",
+        c: "Stormtrooper",
+        d: "Force",
+        correct: "a"
+    },
+    {
+        question: "Anakin Skywalker grew up to be who?",
+        a: "Luke Skywalker",
+        b: "Yoda",
+        c: "Darth Vader",
+        d: "Kylo Ren",
+        correct: "c",
+    },
+]
